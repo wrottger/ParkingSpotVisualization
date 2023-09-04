18f6144b9116b5fd90fe8063e1607fbd4f2f22f8
MoonGyu-Jeong <newsm5403@kookmin.ac.kr>
2023-09-04 12:15:16 +0200

    added config file

---
+++ hall_effect_scripts/config.py
@@ -0,0 +1,6 @@
+STATUS_FILE = "output/status.csv"
+HISTORICAL_STATUSES = "output/historical_status.csv"
+CALIBRATION_FILE = "output/calibrated_values.csv"
+ALLOWED_DEVIATION = 2
+BUFFER_SIZE = 40
+CALIBRATION_SIZE = 200
